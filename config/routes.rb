68455bd5ea639572033788741bb5e9446d1fedbd
NLDN_Nguyen_Van_Phuong <phuong.nv@neo-lab.vn>
2021-12-07 14:24:34 +0700

    register and sign in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/create'
-  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
 
   resources :posts
   devise_for :users
 
-  post 'access' => 'users#access', as: :create_session
+  post 'authen' => 'users#create', as: :create_session
   get 'share' => 'posts#new', as: :share_movie
 end
